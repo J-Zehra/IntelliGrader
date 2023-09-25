8ef7818c1ec8681735220e950195f754b2e6942d
Jad <jazencode@gmail.com>
2023-09-26 05:48:02 +0800

    added fixf

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,51 @@
 
 import { Button, Link, Stack, Text } from "@chakra-ui/react";
 import { IoMdAdd } from "react-icons/io";
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import ClassesList from "@/components/classesList";
 import ClassLoading from "@/components/classLoading";
 import CustomContainer from "@/components/reusables/customContainer";
 import EmptyClass from "@/components/emptyClass";
+import { FetchedClassInfo } from "@/utils/types";
 
 export default function Home() {
-  const { data, isLoading, isSuccess } = useQuery({
-    queryKey: ["classes"],
-    queryFn: async () => {
-      const res = await fetch("/api/classes", {
-        cache: "no-store",
-        next: { revalidate: 0 },
-      });
-      const classes = res.json();
-      console.log(classes);
+  // const { data, isLoading, isSuccess } = useQuery({
+  //   queryKey: ["classes"],
+  //   queryFn: async () => {
+  //     const res = await fetch("/api/classes", {
+  //       cache: "no-store",
+  //       next: { revalidate: 0 },
+  //     });
+  //     const classes = res.json();
+  //     console.log(classes);
 
-      return classes;
-    },
-    cacheTime: 0,
-    staleTime: 0,
-  });
+  //     return classes;
+  //   },
+  //   cacheTime: 0,
+  //   staleTime: 0,
+  // });
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isSuccess, setSuccess] = useState<boolean>(false);
+  const [data, setData] = useState<FetchedClassInfo[]>([]);
+
+  useEffect(() => {
+    const getClasses = async () => {
+      setIsLoading(true);
+      const res = await axios("/api/classes");
+      setIsLoading(false);
+      setSuccess(true);
+      const classes = res.data as unknown as FetchedClassInfo[];
+      console.log(classes);
+      setData(classes);
+    };
+
+    getClasses();
+  }, []);
+
+  console.log(data);
 
   const renderClasses = () => {
     if (isLoading) {
