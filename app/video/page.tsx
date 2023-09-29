0941851851d80da0611319e69abb57b7bc5e97f8
Jad <jazencode@gmail.com>
2023-09-29 11:55:41 +0800

    try

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,15 +1,43 @@
+/* eslint-disable jsx-a11y/media-has-caption */
+
 "use client";
 
-import { Box, Image } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
+// import { io } from "socket.io-client";
 
 export default function VideoPage() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  // const socket = io("http://your-flask-server-address");
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          width: 1920,
+          height: 1080,
+          // facingMode: { exact: "environment" },
+        },
+      })
+      .then((stream) => {
+        const video = videoRef.current;
+        if (video) {
+          video.srcObject = stream;
+          video.play();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
   return (
-    <Box bg="rgba(0, 0, 0, .1)" h="80vh">
-      <Image
-        src="https://jazen.pythonanywhere.com/video_feed"
-        w="100%"
-        h="100%"
-      />
+    <Box w="100%" bg="rgba(0, 0, 0, .1)" h="100%">
+      <video ref={videoRef} />;
     </Box>
   );
 }
