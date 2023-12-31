4fca1923df65a20a191487794e96eb1e9a4545c8
Jad <jazencode@gmail.com>
2023-12-31 08:06:32 +0800

    change socket

--- app/(class)/[class_id]/(tests)/[test_id]/socket.ts
+++ app/(class)/[class_id]/(tests)/[test_id]/socket.ts
@@ -1,8 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import { io } from "socket.io-client";
 
-// export const socket = io(
-//   "https://intelli-grader-backend-43b270ab373f.herokuapp.com/",
-// );
+export const socket = io(
+  "https://intelli-grader-backend-43b270ab373f.herokuapp.com/",
+);
 
-export const socket = io("http://127.0.0.1:5000/");
+// export const socket = io("http://127.0.0.1:5000/");
