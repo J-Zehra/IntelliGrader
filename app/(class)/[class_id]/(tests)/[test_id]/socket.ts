1efbe27e9f3fe54daabff958c7d2bf96c34c1a0d
Jad <jazencode@gmail.com>
2024-01-04 10:31:57 +0800

    change sockety

--- app/(class)/[class_id]/(tests)/[test_id]/socket.ts
+++ app/(class)/[class_id]/(tests)/[test_id]/socket.ts
@@ -2,7 +2,11 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  "https://intelli-grader-backend-43b270ab373f.herokuapp.com/",
+  "https://intelli-grader-b-b4886838bad3.herokuapp.com/",
 );
+
+// export const socket = io(
+//   "https://intelli-grader-backend-43b270ab373f.herokuapp.com/",
+// );
 
 // export const socket = io("http://127.0.0.1:5000/");
