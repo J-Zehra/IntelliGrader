8a9857cbcb95e1c96cc335d17bf72f8a998245fa
Jad <jazencode@gmail.com>
2024-01-04 11:28:23 +0800

    test

--- app/(class)/[class_id]/(tests)/[test_id]/video/page.tsx
+++ app/(class)/[class_id]/(tests)/[test_id]/video/page.tsx
@@ -1,42 +1,42 @@
 "use client";
 
-import { Box, Image, Stack } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
-import { useSetRecoilState } from "recoil";
-import { localGradeInfo } from "@/state/localGradeInfo";
-import { useRouter } from "next/navigation";
+// import { useSetRecoilState } from "recoil";
+// import { localGradeInfo } from "@/state/localGradeInfo";
+// import { useRouter } from "next/navigation";
 import { socket } from "../socket";
 
 export default function VideoPage() {
-  const setLocalGradesInfo = useSetRecoilState(localGradeInfo);
-  const [rollNumberSection, setRollNumberSection] = useState("");
-  const [bubbleSection, setBubbleSection] = useState("");
-  const navigate = useRouter();
+  // const setLocalGradesInfo = useSetRecoilState(localGradeInfo);
+  // const [rollNumberSection, setRollNumberSection] = useState("");
+  // const [bubbleSection, setBubbleSection] = useState("");
+  // const navigate = useRouter();
   const [openCamera, setOpenCamera] = useState<boolean>(true);
   const webcamRef = useRef<Webcam>(null);
 
   useEffect(() => {
     const captureFrame = () => {
       const imageSrc = webcamRef.current?.getScreenshot();
-      if (imageSrc && !rollNumberSection && !bubbleSection) {
-        socket.emit("image", imageSrc, (data: any) => {
-          if (data.status === "success") {
+      if (imageSrc && openCamera) {
+        socket.emit("image", imageSrc, (cb: any) => {
+          if (cb.status === "success") {
             setOpenCamera(false);
-            setRollNumberSection(data.rollNumberSection);
-            setBubbleSection(data.bubbleSection);
+            // setRollNumberSection(data.rollNumberSection);
+            // setBubbleSection(data.bubbleSection);
 
-            const testData = {
-              rollNumberSection: data.rollNumberSection,
-              bubbleSection: data.bubbleSection,
-              answer: [0, 1, 2, 2, 3, 3, 3],
-              numberOfChoices: [5, 5, 5, 5],
-            };
+            // const testData = {
+            //   rollNumberSection: data.rollNumberSection,
+            //   bubbleSection: data.bubbleSection,
+            //   answer: [0, 1, 2, 2, 3, 3, 3],
+            //   numberOfChoices: [5, 5, 5, 5],
+            // };
 
-            socket.emit("single_grade", testData, (processedTestData: any) => {
-              setLocalGradesInfo(processedTestData);
-              navigate.push("local_student_grades");
-            });
+            // socket.emit("single_grade", testData, (processedTestData: any) => {
+            //   setLocalGradesInfo(processedTestData);
+            //   navigate.push("local_student_grades");
+            // });
           }
         });
       }
@@ -45,7 +45,7 @@ export default function VideoPage() {
     const interval = setInterval(captureFrame, 100);
 
     return () => clearInterval(interval);
-  }, [bubbleSection, navigate, rollNumberSection, setLocalGradesInfo]);
+  }, [openCamera]);
 
   return (
     <Box
@@ -70,7 +70,8 @@ export default function VideoPage() {
         />
       ) : (
         <Stack w="100%" h="100%" p="1rem">
-          <Image
+          <Text>Proccesing</Text>
+          {/* <Image
             alt="Processed Image"
             src={`data:image/jpeg;base64, ${bubbleSection}`}
             width={500}
@@ -89,7 +90,7 @@ export default function VideoPage() {
               width: "100%",
               height: "100%",
             }}
-          />
+          /> */}
         </Stack>
       )}
     </Box>
