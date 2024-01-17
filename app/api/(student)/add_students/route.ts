00ea533a6fb068b3667b0b043f94777895f92ac8
Jad <jazencode@gmail.com>
2024-01-17 19:38:39 +0800

    fix roll number issue

--- app/api/(student)/add_students/route.ts
+++ app/api/(student)/add_students/route.ts
@@ -18,6 +18,7 @@ export async function POST(request: Request) {
     };
 
     const lastRollNumber = await prisma.student.aggregate({
+      where: { classId },
       _max: { rollNumber: true },
     });
 
